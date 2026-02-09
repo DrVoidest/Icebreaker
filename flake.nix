799e341097074c3e632bb62ec21b97cddfa8fe9d
Dr. Void <dr.void_industries@proton.me>
2026-02-09 10:38:17 -0500

    added flake to make developing easier

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Project Icebreaker flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        hello
+      ];
+      shellHook = ''
+        echo "Welcome to the Icebreaker Dev Shell"
+      '';
+    };
+  };
+}
